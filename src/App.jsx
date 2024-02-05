7f5feb885bae06679e1933bfba82d4c97e1efeb1
paras <parasad612@gmail.com>
2024-02-05 02:05:10 +0100

    widee contact bar

--- src/App.jsx
+++ src/App.jsx
@@ -14,32 +14,30 @@ const App = () => {
         <div className="min-h-screen bg-pastel-green text-gray-800 flex flex-col justify-center items-center">
 
           {/* Main Content */}
-          <section className="container mx-auto mt-8 flex justify-center">
-            <div className="max-w-lg p-8 bg-white shadow-lg rounded-md border border-green-500">
-              <h1 className="text-4xl font-bold text-gray-800 mb-4">Paras Adhikari</h1>
-              <p className="text-gray-600 mb-6">Software Developer</p>
-              <div className="bg-green-100 p-6 rounded-md mb-8">
-                <h2 className="text-lg font-bold text-gray-800 mb-2">About Me</h2>
-                <p className="text-gray-600">I'm a passionate Software Developer with a keen interest in creating user-friendly programs and applications.</p>
-              </div>
-              <div className="bg-green-100 p-6 rounded-md">
-                <h2 className="text-lg font-bold text-gray-800">Skills</h2>
-                <ul className=" text-gray-600 list-none">
-                  <li>Java</li>
-                  <li>Python</li>
-                  <li>JavaScript</li>
-                  <li>React</li>
-                  <li>Tailwind CSS</li>
-                </ul>
-              </div>
+          <div className="max-w-lg p-8 bg-white shadow-lg rounded-md border border-green-500">
+            <h1 className="text-4xl font-bold text-gray-800 mb-4">Paras Adhikari</h1>
+            <p className="text-gray-600 mb-6">Software Developer</p>
+            <div className="bg-green-100 p-6 rounded-md mb-8">
+              <h2 className="text-lg font-bold text-gray-800 mb-2">About Me</h2>
+              <p className="text-gray-600">I'm a passionate Software Developer with a keen interest in creating user-friendly programs and applications.</p>
             </div>
-          </section>
+            <div className="bg-green-100 p-6 rounded-md">
+              <h2 className="text-lg font-bold text-gray-800">Skills</h2>
+              <ul className=" text-gray-600 list-none">
+                <li>Java</li>
+                <li>Python</li>
+                <li>JavaScript</li>
+                <li>React</li>
+                <li>Tailwind CSS</li>
+              </ul>
+            </div>
+          </div>
 
 
           {/* Contact Bar */}
-          <footer className="bg-white shadow-md p-4 mt-8 rounded-lg">
+          <footer className="bg-white shadow-md p-4 mt-8 rounded-lg max-w-lg border border-green-500">
             <p className="text-lg font-semibold">Contact</p>
-            <ul className="flex space-x-4 mt-2 underline">
+            <ul className="flex space-x-40 mt-2 underline">
               <li>
                 <button className="underline" onClick={toggleView}>Email</button>
               </li>
